1154e98df6c66be2d894dc5a4cfb9b39b2f475c1
Solanyi Aldana <solan1998@hotmail.com>
2025-02-20 12:17:14 -0500

    1.Validación de nombre

---
+++ app.js
@@ -0,0 +1,18 @@
+//*Variables globales//*
+// Seleccionar elementos del DOM
+let numeroMaximo = 10;
+
+//array
+let amigos = [];
+
+//*Agregar nombre y verifcar que no este vacio
+function validarNombre() {
+    let name = document.getElementById(amigo).value;
+    if ( name === "") {
+        alert("Añade un nombre válido")
+    } else {
+        console.log("Nombre añadido:" + name)
+    }
+    return
+}
+
